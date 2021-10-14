adc07c0afb8b00f206547fd1328a43d893900876
EricPrieto <ep1043@hotmail.com>
2021-10-14 10:53:12 -0500

    Tested Soda Machine class function determine_change_value

--- test_soda_machine.py
+++ test_soda_machine.py
@@ -17,6 +17,86 @@ class TestFillInventory(unittest.TestCase):
         self.assertEqual(30, len(self.soda_machine.inventory))
         
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TestDetermineChangeValue(unittest.TestCase):
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+    def test_with_higher_payment(self):
+        return_a = self.soda_machine.determine_change_value(.80,0)
+        return_b = self.soda_machine.determine_change_value(0,.60)
+        self.assertGreater(return_a,return_b) 
+
+    def test_with_soda_price_higher(self):
+        return_a = self.soda_machine.determine_change_value(0,.60)
+        return_b = self.soda_machine.determine_change_value(.30,0)
+        self.assertLess (return_a,return_b)
+
+    def test_soda_equal_payment(self):
+        return_a = self.soda_machine.determine_change_value(0,0)
+        return_b = self.soda_machine.determine_change_value(0,0)
+        self.assertEqual(return_a,return_b)   
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
 
